948cf387d7574df868813ca1ba56c7ccea6b506b
karthick09 <karthickmtvm@gmail.com>
2022-03-16 15:00:59 +0530

    new file

--- place.java
+++ place.java
@@ -2,12 +2,7 @@ public class place{
     public static void main(String[] args) {
         try
         {
-            int[] x = new int [3];
-            x[4]=100;
             
-
-
-
         }
     }
     
